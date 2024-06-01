496047a5d08011bc919f0742199776a12fb6074f
paul alexander diaz velez <108167418+poldiaz50@users.noreply.github.com>
2024-06-01 03:11:56 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/* typing animation */
+var typed = new Typed(".typing", {
+  strings: [
+    "",
+    "Diseñador de Software",
+    "Desarrollador Web",
+    "Diseñador de DB",
+    "CO de POLSystems",
+  ],
+  typeSpeed: 100,
+  BackSpeed: 60,
+  loop: true,
+});
